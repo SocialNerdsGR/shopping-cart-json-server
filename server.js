9f6829e71c38bfb7f0839749ea36a4f73c99bca8
Thanos Korakas <thanoskorakas@gmail.com>
2019-10-06 20:39:14 +0300

    Added authentication

--- server.js
+++ server.js
@@ -2,7 +2,30 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+const key = process.env.API_KEY;
+
+function isAuthorized(request) {
+  const api_key = request.query.api_key;
+  if (!api_key) {
+    console.log("Missing API key");
+    return false;
+  }
+  if (api_key !== key) {
+    console.log("Wrong API key");
+    return false;
+  }
+
+  return true;
+}
+
+server.use((req, res, next) => {
+  if (!isAuthorized(req)) {
+    res.sendStatus(401);
+  }
+
+  next();
+});
 
 server.use(middlewares);
 server.use(router);
